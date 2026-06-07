c2917f952fea3aee4101dc2f1157a66f1477f105
Tracy <trac.boats.tb@gmail.com>
2026-06-07 16:49:34 -0500

    app.js / useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import StarRating from "./components/StarRating";
+import Dialog from "./components/Dialog";
 
 function App() {
-  return (
-<></>
-  );
+
+    const [showDialog, setShowDialog] = useState(true);
+
+    return (
+        <main>
+
+            {showDialog && (
+                <div className="dialog">
+                    <StarRating />
+                </div>
+            )}
+
+            <Dialog
+                closeDialog={() => setShowDialog(false)}
+            />
+
+        </main>
+    );
 }
 
 export default App;
